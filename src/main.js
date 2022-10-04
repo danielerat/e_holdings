c32c4aff56106b0c5018690be1f28461e42645d8
danielerat <davidodo2015@gmail.com>
2022-10-04 10:35:16 +0200

    Tailwind Css Configuration Sample

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import "./assets/tailwind.css";
-import store from "./store";
-import router from "./router";
 
-createApp(App).use(router).use(store).mount("#app");
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+// Registering FontAwesomeIcon TO be used in the site
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
+library.add(faCartPlus);
+
+import "./assets/tailwind.css";
+import App from "./App.vue";
+
+import router from "@/router";
+
+createApp(App)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    "./public/**/*.html",
+    "./node_modules/flowbite/**/*.js",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        //Adding google fonts
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        //Addin our custom color
+        "site-gray-1": "#102A43",
+        "site-gray-2": "#334E68",
+        "site-gray-3": "#627D98",
+        "site-green-1": "#014D40",
+        "site-green-2": "#27AB83",
+        "site-green-3": "#3EBD93",
+        "site-white-1": "#F0F4F8",
+        "site-white-2": "#D9E2EC",
+        "site-white-3": "#BCCCDC",
+      },
+      boxShadow: {
+        blue: "0 0 3px 3px #4285f4",
+      },
+    },
+  },
+  variants: {
+    //Adding first so we can be able to apply designs on a first child
+    margin: ["first"],
   },
   plugins: [],
 };
