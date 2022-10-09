c479f65e923598b759a31297c35244dde0cec18e
danielerat <davidodo2015@gmail.com>
2022-10-09 06:30:24 +0200

    Adding admin Components -DrowDowns -Footer -Header- -Side navbar

---
+++ src/components/Admin/Navbars/AdminNavbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- Navbar -->
+  <nav
+    class="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4"
+  >
+    <div
+      class="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4"
+    >
+      <!-- Brand -->
+      <a
+        class="text-white text-sm uppercase hidden lg:inline-block font-semibold"
+        href="javascript:void(0)"
+      >
+        Dashboard
+      </a>
+
+      <!-- User -->
+      <ul class="flex-col md:flex-row list-none items-center hidden md:flex">
+        <user-dropdown />
+      </ul>
+    </div>
+  </nav>
+  <!-- End Navbar -->
+</template>
+
+<script>
+import UserDropdown from "@/components/Admin/Dropdowns/AdminUserDropdown";
+export default {
+  components: {
+    UserDropdown,
+  },
+};
+</script>
---
+++ src/components/Admin/Sidebar/AdminSidebar.vue
@@ -0,0 +1,170 @@
+<template>
+  <nav
+    class="md:left-0 text-left md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-site-white-5 flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6"
+  >
+    <div
+      class="md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto"
+    >
+      <!-- Toggler -->
+      <button
+        class="cursor-pointer text-site-gray-1 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border-solid border-transparent"
+        type="button"
+        v-on:click="toggleCollapseShow('bg-white m-2 py-3 px-6')"
+      >
+        <fa :icon="['fa', 'bars']" class="text-site-gray-1 text-2xl" />
+      </button>
+      <!-- Brand -->
+      <router-link
+        class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
+        to="/"
+      >
+        E-Holdings
+      </router-link>
+      <!-- User -->
+      <ul class="md:hidden items-center flex flex-wrap list-none">
+        <li class="inline-block relative">
+          <notification-dropdown />
+        </li>
+        <li class="inline-block relative">
+          <user-dropdown />
+        </li>
+      </ul>
+      <!-- Collapse -->
+      <div
+        class="md:flex md:flex-col md:items-stretch md:opacity-100 md:relative md:mt-4 md:shadow-none shadow absolute top-0 left-0 right-0 z-40 overflow-y-auto overflow-x-hidden h-auto items-center flex-1 rounded"
+        v-bind:class="collapseShow"
+      >
+        <!-- Collapse header -->
+        <div
+          class="md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-blueGray-200"
+        >
+          <div class="flex flex-wrap">
+            <div class="w-6/12">
+              <router-link
+                class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
+                to="/"
+              >
+                E-Holdings
+              </router-link>
+            </div>
+            <div class="w-6/12 flex justify-end">
+              <button
+                type="button"
+                class="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border-solid border-transparent"
+                v-on:click="toggleCollapseShow('hidden')"
+              >
+                <fa :icon="['fa', 'times']" class="text-site-green-1 text-xl" />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <!-- Heading -->
+        <h6
+          class="md:min-w-full text-site-gray-4 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+        >
+          Admin Layout Pages
+        </h6>
+        <!-- Navigation -->
+
+        <ul
+          class="md:flex-co justify-start md:min-w-full flex flex-col list-none"
+        >
+          <li class="items-start">
+            <router-link to="/admin" v-slot="{ href, navigate, isActive }">
+              <a
+                :href="href"
+                @click="navigate"
+                class="text-xs uppercase py-3 font-bold block"
+                :class="[
+                  isActive
+                    ? 'text-site-green-1 hover:text-site-green-2'
+                    : 'text-site-gray-1 hover:text-site-gray-5',
+                ]"
+              >
+                <fa
+                  :icon="['fa', 'gauge']"
+                  class="text-xl"
+                  :class="[isActive ? 'text-site-green-1' : 'opacity-75']"
+                ></fa>
+                Dashboard
+              </a>
+            </router-link>
+          </li>
+          <li class="items-center">
+            <router-link
+              to="/admin/devices"
+              v-slot="{ href, navigate, isActive }"
+            >
+              <a
+                :href="href"
+                @click="navigate"
+                class="text-xs uppercase py-3 font-bold block"
+                :class="[
+                  isActive
+                    ? 'text-site-green-1 hover:text-site-green-2'
+                    : 'text-site-gray-1 hover:text-site-gray-5',
+                ]"
+              >
+                <fa
+                  :icon="['fa', 'gears']"
+                  class="text-xl"
+                  :class="[isActive ? 'text-site-green-1' : 'opacity-75']"
+                ></fa>
+                Devices
+              </a>
+            </router-link>
+          </li>
+          <li class="items-center">
+            <router-link
+              to="/admin/settings"
+              v-slot="{ href, navigate, isActive }"
+            >
+              <a
+                :href="href"
+                @click="navigate"
+                class="text-xs uppercase py-3 font-bold block"
+                :class="[
+                  isActive
+                    ? 'text-site-green-1 hover:text-site-green-2'
+                    : 'text-site-gray-1 hover:text-site-gray-5',
+                ]"
+              >
+                <fa
+                  :icon="['fa', 'gears']"
+                  class="text-xl"
+                  :class="[isActive ? 'text-site-green-1' : 'opacity-75']"
+                ></fa>
+                Settings
+              </a>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+); }
+
+<script>
+import NotificationDropdown from "@/components/Admin/Dropdowns/AdminNotificationDropdown";
+import UserDropdown from "@/components/Admin/Dropdowns/AdminUserDropdown.vue";
+
+export default {
+  data() {
+    return {
+      //   collapseShow: "bg-white m-2 py-3 px-6",
+      collapseShow: "hidden",
+    };
+  },
+  methods: {
+    toggleCollapseShow: function (classes) {
+      this.collapseShow = classes;
+    },
+  },
+  components: {
+    NotificationDropdown,
+    UserDropdown,
+  },
+};
+</script>
