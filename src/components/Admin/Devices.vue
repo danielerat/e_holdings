1535d302deef90980e3c004aa223d3740bae8c52
danielerat <davidodo2015@gmail.com>
2022-11-18 22:02:44 +0200

    Adding the device page

---
+++ src/components/Admin/Devices.vue
@@ -0,0 +1,341 @@
+<template>
+  <div class="flex flex-col md:flex-row justify-around flex-wrap">
+    <div
+      class="basis-1/4 relative rounded-xl bg-gradient-to-tr from-site-white-4 to-site-white-5 border border-site-white-3 hover:bg-gradient-to-br cursor-pointer p-8 shadow-xl mx-5 my-5 bg-contain bg-no-repeat"
+      :style="{ backgroundImage: 'url(' + image + ')' }"
+    >
+      <span
+        class="absolute top-4 rounded-full py-1.5 text-xs font-medium text-gray-1"
+      >
+        Since: July,15,2022
+      </span>
+      <span
+        class="absolute right-4 top-4 rounded-full bg-green-100 px-3 py-1.5 text-xs font-medium text-green-600"
+      >
+        Active
+      </span>
+
+      <div class="mt-4 text-gray-500 sm:pr-8">
+        <div class="flex justify-around">
+          <p>
+            <fa icon="laptop" class="text-xl" />
+          </p>
+          <p class="text-xs">SN/IMEI: S14525MKGP3572</p>
+        </div>
+
+        <h3 class="mt-4 text-sm text-site-gray-1">
+          <p class="font-bold">Apple MacBook Pro Gen 13</p>
+
+          <p class="text-xs text-site-gray-2">Model: MacBookPro18,3</p>
+
+          <p></p>
+        </h3>
+
+        <p class="mt-2 hidden text-xs sm:block">
+          Lorem whatever is the what that we actually care about nothing else...
+        </p>
+
+        <div class="flex mt-5 text-lg sm:block">
+          <div class="flex">
+            <div
+              class="border-r-2 px-2 text-site-green-3 hover:text-site-green-1"
+            >
+              <fa icon="eye" />
+            </div>
+            <div
+              class="border-r-2 px-2 text-site-green-3 hover:text-site-green-1"
+            >
+              <fa icon="gauge" />
+            </div>
+            <div class="px-2 text-site-green-3 hover:text-site-green-1">
+              <fa icon="share" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="basis-1/4 relative rounded-xl bg-gradient-to-tr from-site-white-4 to-site-white-5 border border-site-white-3 hover:bg-gradient-to-br cursor-pointer p-8 shadow-xl mx-5 my-5 bg-contain bg-no-repeat"
+      :style="{ backgroundImage: 'url(' + waveyellow + ')' }"
+    >
+      <span
+        class="absolute top-4 rounded-full py-1.5 text-xs font-medium text-gray-1"
+      >
+        Since: July,15,2022
+      </span>
+      <span
+        class="absolute right-4 top-4 rounded-full bg-site-yellow-4 px-3 py-1.5 text-xs font-medium text-site-yellow-1"
+      >
+        Lost
+      </span>
+
+      <div class="mt-4 text-gray-500 sm:pr-8">
+        <div class="flex justify-around">
+          <p>
+            <fa icon="mobile-button" class="text-xl" />
+          </p>
+          <p class="text-xs">SN/IMEI: S14525MKGP3572</p>
+        </div>
+
+        <h3 class="mt-4 text-sm text-site-gray-1">
+          <p class="font-bold">Apple MacBook Pro Gen 13</p>
+
+          <p class="text-xs text-site-gray-2">Model: MacBookPro18,3</p>
+
+          <p></p>
+        </h3>
+
+        <p class="mt-2 hidden text-xs sm:block">
+          Lorem whatever is the what that we actually care about nothing else...
+        </p>
+
+        <div class="flex mt-5 text-lg sm:block">
+          <div class="flex">
+            <div
+              class="border-r-2 px-2 text-site-green-3 hover:text-site-green-1"
+            >
+              <fa icon="eye" />
+            </div>
+            <div
+              class="border-r-2 px-2 text-site-green-3 hover:text-site-green-1"
+            >
+              <fa icon="gauge" />
+            </div>
+            <div class="px-2 text-site-green-3 hover:text-site-green-1">
+              <fa icon="share" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="basis-1/4 relative rounded-xl bg-gradient-to-tr from-site-white-4 to-site-white-5 border border-site-white-3 hover:bg-gradient-to-br cursor-pointer p-8 shadow-xl mx-5 my-5 bg-contain bg-no-repeat"
+      :style="{ backgroundImage: 'url(' + wave + ')' }"
+    >
+      <span
+        class="absolute top-4 rounded-full py-1.5 text-xs font-medium text-gray-1"
+      >
+        Since: July,15,2022
+      </span>
+      <span
+        class="absolute right-4 top-4 rounded-full bg-green-100 px-3 py-1.5 text-xs font-medium text-green-600"
+      >
+        Active
+      </span>
+
+      <div class="mt-4 text-gray-500 sm:pr-8">
+        <div class="flex justify-around">
+          <p>
+            <fa icon="laptop" class="text-xl" />
+          </p>
+          <p class="text-xs">SN/IMEI: S14525MKGP3572</p>
+        </div>
+
+        <h3 class="mt-4 text-sm text-site-gray-1">
+          <p class="font-bold">Apple MacBook Pro Gen 13</p>
+
+          <p class="text-xs text-site-gray-2">Model: MacBookPro18,3</p>
+
+          <p></p>
+        </h3>
+
+        <p class="mt-2 hidden text-xs sm:block">
+          Lorem whatever is the what that we actually care about nothing else...
+        </p>
+
+        <div class="flex mt-5 text-lg sm:block">
+          <div class="flex">
+            <div
+              class="border-r-2 px-2 text-site-green-3 hover:text-site-green-1"
+            >
+              <fa icon="eye" />
+            </div>
+            <div
+              class="border-r-2 px-2 text-site-green-3 hover:text-site-green-1"
+            >
+              <fa icon="gauge" />
+            </div>
+            <div class="px-2 text-site-green-3 hover:text-site-green-1">
+              <fa icon="share" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="basis-1/4 relative rounded-xl bg-gradient-to-tr from-site-white-4 to-site-white-5 border border-site-white-3 hover:bg-gradient-to-br cursor-pointer p-8 shadow-xl mx-5 my-5 bg-contain bg-no-repeat"
+      :style="{ backgroundImage: 'url(' + wave + ')' }"
+    >
+      <span
+        class="absolute top-4 rounded-full py-1.5 text-xs font-medium text-gray-1"
+      >
+        Since: July,15,2022
+      </span>
+      <span
+        class="absolute right-4 top-4 rounded-full bg-green-100 px-3 py-1.5 text-xs font-medium text-green-600"
+      >
+        Active
+      </span>
+
+      <div class="mt-4 text-gray-500 sm:pr-8">
+        <div class="flex justify-around">
+          <p>
+            <fa icon="laptop" class="text-xl" />
+          </p>
+          <p class="text-xs">SN/IMEI: S14525MKGP3572</p>
+        </div>
+
+        <h3 class="mt-4 text-sm text-site-gray-1">
+          <p class="font-bold">Apple MacBook Pro Gen 13</p>
+
+          <p class="text-xs text-site-gray-2">Model: MacBookPro18,3</p>
+
+          <p></p>
+        </h3>
+
+        <p class="mt-2 hidden text-xs sm:block">
+          Lorem whatever is the what that we actually care about nothing else...
+        </p>
+
+        <div class="flex mt-5 text-lg sm:block">
+          <div class="flex">
+            <div
+              class="border-r-2 px-2 text-site-green-3 hover:text-site-green-1"
+            >
+              <fa icon="eye" />
+            </div>
+            <div
+              class="border-r-2 px-2 text-site-green-3 hover:text-site-green-1"
+            >
+              <fa icon="gauge" />
+            </div>
+            <div class="px-2 text-site-green-3 hover:text-site-green-1">
+              <fa icon="share" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="basis-1/4 relative rounded-xl bg-gradient-to-tr from-site-white-4 to-site-white-5 border border-site-white-3 hover:bg-gradient-to-br cursor-pointer p-8 shadow-xl mx-5 my-5 bg-contain bg-no-repeat"
+      :style="{ backgroundImage: 'url(' + wave + ')' }"
+    >
+      <span
+        class="absolute top-4 rounded-full py-1.5 text-xs font-medium text-gray-1"
+      >
+        Since: July,15,2022
+      </span>
+      <span
+        class="absolute right-4 top-4 rounded-full bg-green-100 px-3 py-1.5 text-xs font-medium text-green-600"
+      >
+        Active
+      </span>
+
+      <div class="mt-4 text-gray-500 sm:pr-8">
+        <div class="flex justify-around">
+          <p>
+            <fa icon="laptop" class="text-xl" />
+          </p>
+          <p class="text-xs">SN/IMEI: S14525MKGP3572</p>
+        </div>
+
+        <h3 class="mt-4 text-sm text-site-gray-1">
+          <p class="font-bold">Apple MacBook Pro Gen 13</p>
+
+          <p class="text-xs text-site-gray-2">Model: MacBookPro18,3</p>
+
+          <p></p>
+        </h3>
+
+        <p class="mt-2 hidden text-xs sm:block">
+          Lorem whatever is the what that we actually care about nothing else...
+        </p>
+
+        <div class="flex mt-5 text-lg sm:block">
+          <div class="flex">
+            <div
+              class="border-r-2 px-2 text-site-green-3 hover:text-site-green-1"
+            >
+              <fa icon="eye" />
+            </div>
+            <div
+              class="border-r-2 px-2 text-site-green-3 hover:text-site-green-1"
+            >
+              <fa icon="gauge" />
+            </div>
+            <div class="px-2 text-site-green-3 hover:text-site-green-1">
+              <fa icon="share" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="basis-1/4 relative rounded-xl bg-gradient-to-tr from-site-white-4 to-site-white-5 border border-site-white-3 hover:bg-gradient-to-br cursor-pointer p-8 shadow-xl mx-5 my-5 bg-contain bg-no-repeat"
+      :style="{ backgroundImage: 'url(' + waveyellow + ')' }"
+    >
+      <span
+        class="absolute top-4 rounded-full py-1.5 text-xs font-medium text-gray-1"
+      >
+        Since: July,15,2022
+      </span>
+      <span
+        class="absolute right-4 top-4 rounded-full bg-site-yellow-4 px-3 py-1.5 text-xs font-medium text-site-yellow-1"
+      >
+        Lost
+      </span>
+
+      <div class="mt-4 text-gray-500 sm:pr-8">
+        <div class="flex justify-around">
+          <p>
+            <fa icon="laptop" class="text-xl" />
+          </p>
+          <p class="text-xs">SN/IMEI: S14525MKGP3572</p>
+        </div>
+
+        <h3 class="mt-4 text-sm text-site-gray-1">
+          <p class="font-bold">Apple MacBook Pro Gen 13</p>
+
+          <p class="text-xs text-site-gray-2">Model: MacBookPro18,3</p>
+
+          <p></p>
+        </h3>
+
+        <p class="mt-2 hidden text-xs sm:block">
+          Lorem whatever is the what that we actually care about nothing else...
+        </p>
+
+        <div class="flex mt-5 text-lg sm:block">
+          <div class="flex">
+            <div
+              class="border-r-2 px-2 text-site-green-3 hover:text-site-green-1"
+            >
+              <fa icon="eye" />
+            </div>
+            <div
+              class="border-r-2 px-2 text-site-green-3 hover:text-site-green-1"
+            >
+              <fa icon="gauge" />
+            </div>
+            <div class="px-2 text-site-green-3 hover:text-site-green-1">
+              <fa icon="share" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Devices",
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/views/admin/Devices.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <sidebar />
+    <div class="relative md:ml-64 bg-site-white-4">
+      <admin-navbar path="List of all Invoices " down="true" />
+
+      <div class="flex justify-around flex-wrap mb-6">
+        <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
+          <card-stats
+            statSubtitle="Sample"
+            statTitle="350,897"
+            statArrow="up"
+            statPercent="3.48"
+            statPercentColor="text-emerald-500"
+            statDescripiron="Since last month"
+            statIconName="chart-bar"
+            statIconColor="bg-site-yellow-1"
+          />
+        </div>
+        <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
+          <card-stats
+            statSubtitle="Sample USERS"
+            statTitle="2,356"
+            statArrow="down"
+            statPercent="3.48"
+            statPercentColor="text-red-500"
+            statDescripiron="Since last week"
+            statIconName="chart-pie"
+            statIconColor="bg-site-gray-2"
+          />
+        </div>
+
+        <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
+          <card-stats
+            statSubtitle="Sample"
+            statTitle="49,65%"
+            statArrow="up"
+            statPercent="12"
+            statPercentColor="text-emerald-500"
+            statDescripiron="Since last month"
+            statIconName="percent"
+            statIconColor="bg-site-green-2"
+          />
+        </div>
+      </div>
+      <!-- Settings -->
+      <div class="flex flex-wrap justify-center align-baseline">
+        <devices />
+      </div>
+      <!-- ---- -->
+      <div class="px-4 md:px-10 mx-auto w-full">
+        <footer-admin />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import AdminNavbar from "@/components/Admin/Navbars/AdminNavbar.vue";
+import Sidebar from "@/components/Admin/Sidebar/AdminSidebar.vue";
+import FooterAdmin from "@/components/Admin/Footers/AdminFooter.vue";
+
+import CardStats from "@/components/Cards/CardStats.vue";
+import Devices from "@/components/Admin/Devices.vue";
+export default {
+  name: "AssignDevice",
+  components: {
+    Devices,
+    AdminNavbar,
+    Sidebar,
+    FooterAdmin,
+    CardStats,
+  },
+};
+</script>
