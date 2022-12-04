eba9fab14427c55c8b5e093ecd5a3339306a2047
danielerat <davidodo2015@gmail.com>
2022-12-04 21:57:33 +0200

    Adding Step Ui Component

---
+++ src/components/shared/StepMark.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div
+      class="after:mt-4 after:block after:h-1 after:w-full after:rounded-lg after:bg-gray-200"
+    >
+      <ol class="grid grid-cols-3 text-sm font-medium">
+        <li class="relative text-left">
+          <span
+            class="absolute left-0 -bottom-[1.75rem] rounded-full border-2 border-site-green-2 bg-site-green-3 text-white"
+          >
+            <fa icon="check" class="px-1" />
+          </span>
+
+          <span class="font-bold hidden sm:block">Device</span>
+
+          <fa
+            icon="mobile-button"
+            class="ml-auto h-6 w-6 sm:hidden text-site-green-3"
+          />
+        </li>
+        <!--------------------------------- -->
+
+        <li class="relative text-center">
+          <span
+            class="absolute left-1/2 -bottom-[1.75rem] -translate-x-1/2 rounded-full border-2 border-site-green-2 bg-site-green-2 text-white"
+          >
+            <fa icon="xmark-circle" class="px-1" />
+          </span>
+
+          <span class="font-bold hidden sm:block">Owner</span>
+
+          <fa
+            icon="fingerprint"
+            class="ml-auto h-6 w-6 sm:hidden text-site-green-3"
+          />
+        </li>
+
+        <li class="relative text-right">
+          <span
+            class="absolute left-2/2 -bottom-[1.75rem] rounded-full border-2 border-site-gray-4 bg-site-gray-3 text-site-gray-4"
+          >
+            <fa icon="check" class="px-1" />
+          </span>
+          <span class="font-bold hidden sm:block">Sent</span>
+
+          <fa icon="certificate" class="ml-auto h-6 w-6 sm:hidden"></fa>
+        </li>
+      </ol>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "StepMark",
+};
+</script>
