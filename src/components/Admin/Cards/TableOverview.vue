6d0c8cf5046d48bf5b0c320cdf78c9e239114409
danielerat <davidodo2015@gmail.com>
2023-01-12 21:05:39 +0200

    Adding Dashboard Components

---
+++ src/components/Admin/Cards/TableOverview.vue
@@ -0,0 +1,102 @@
+<template>
+  <div
+    class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded"
+  >
+    <div class="rounded-t mb-0 px-2 py-3 border-0">
+      <div class="flex flex-wrap items-center">
+        <div class="relative w-full max-w-full flex-grow flex-1">
+          <p class="font-semibold text-base text-site-gray-2">
+            Devices Under warrantee
+          </p>
+        </div>
+        <div
+          class="relative w-full px-4 max-w-full flex-grow flex-1 text-right"
+        >
+          <button
+            class="bg-site-green-2 text-white active:bg-site-green-2 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            type="button"
+          >
+            See Devices
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="block w-full overflow-x-auto">
+      <!-- Projects table -->
+      <table class="items-center w-full bg-transparent border-collapse">
+        <thead class="thead-light">
+          <tr>
+            <th
+              class="bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
+            >
+              Device Name
+            </th>
+            <th
+              class="bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
+            >
+              Ending Date
+            </th>
+            <th
+              class="bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left min-w-140-px"
+            ></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th
+              class="text border-t-0 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4 text-left"
+            >
+              <p>Iphone 6 Plus GOld</p>
+              <p class="text-xs font-light">To:Ilunga Gisa Daniel</p>
+            </th>
+            <td
+              class="border-t-0 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+            >
+              <span class="font-bold">Ending: December-25-2021</span>
+
+              <progress-bar percent="60"></progress-bar>
+            </td>
+          </tr>
+          <tr>
+            <th
+              class="text border-t-0 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4 text-left"
+            >
+              <p>Smark Iron</p>
+              <p class="text-xs font-light">To:Carl junk</p>
+            </th>
+            <td
+              class="border-t-0 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+            >
+              <span class="font-bold">Ending: December-25-2021</span>
+              <progress-bar color="yellow" percent="20"></progress-bar>
+            </td>
+          </tr>
+
+          <tr>
+            <th
+              class="text border-t-0 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4 text-left"
+            >
+              <p>MacBook Air 32</p>
+              <p class="text-xs font-light">To:Ilunga Gisa Daniel</p>
+            </th>
+            <td
+              class="border-t-0 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+            >
+              <span class="font-bold">Ending: December-25-2021</span>
+              <progress-bar color="gray" percent="20"></progress-bar>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+<script>
+import ProgressBar from "@/components/shared/progress.vue";
+export default {
+  name: "TableDeviceOverview",
+  components: {
+    ProgressBar,
+  },
+};
+</script>
---
+++ src/components/Admin/Cards/TableTransaction.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="flex flex-wrap">
+    <div class="w-full">
+      <ul class="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row">
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a
+            class="text-xs font-bold uppercase px-5 py-5 shadow-lg rounded block leading-normal"
+            v-on:click="toggleTabs(1)"
+            v-bind:class="{
+              'text-emerald-600 bg-white': openTab !== 1,
+              'text-white bg-emerald-600': openTab === 1,
+            }"
+          >
+            <fa icon="spinner" />
+            Pending Devices
+          </a>
+        </li>
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a
+            class="text-xs font-bold uppercase px-5 py-5 shadow-lg rounded block leading-normal"
+            v-on:click="toggleTabs(2)"
+            v-bind:class="{
+              'text-emerald-600 bg-white': openTab !== 2,
+              'text-white bg-emerald-600': openTab === 2,
+            }"
+          >
+            <fa icon="file-contract" />
+            Proof of ownsership
+          </a>
+        </li>
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a
+            class="text-xs font-bold uppercase px-5 py-5 shadow-lg rounded block leading-normal"
+            v-on:click="toggleTabs(3)"
+            v-bind:class="{
+              'text-emerald-600 bg-white': openTab !== 3,
+              'text-white bg-emerald-600': openTab === 3,
+            }"
+          >
+            <fa icon="circle-info" />
+            Reported Devices
+          </a>
+        </li>
+      </ul>
+      <div
+        class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded"
+      >
+        <div class="px-1 py-5 flex-auto">
+          <div class="tab-content tab-space">
+            <div v-bind:class="{ hidden: openTab !== 1, block: openTab === 1 }">
+              <!-- Pending devices table -->
+              <div class="flex flex-wrap items-center">
+                <div class="relative w-full px-4 max-w-full flex-grow flex-1">
+                  <h3 class="font-semibold text-base text-blueGray-700">
+                    Pending devices
+                  </h3>
+                </div>
+                <div
+                  class="relative w-full px-4 max-w-full flex-grow flex-1 text-right"
+                >
+                  <button
+                    class="bg-site-green-2 text-white active:bg-site-green-2 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                  >
+                    See all
+                  </button>
+                </div>
+              </div>
+              <incoming-device
+                v-for="(device, cnt) in devices"
+                :key="device"
+                :name="device.name"
+                :type="device.type"
+                :date="device.date"
+                :model="device.model"
+                :pos="cnt % 2 == 1"
+              ></incoming-device>
+            </div>
+            <div v-bind:class="{ hidden: openTab !== 2, block: openTab === 2 }">
+              <owned-device-proof />
+            </div>
+            <div v-bind:class="{ hidden: openTab !== 3, block: openTab === 3 }">
+              <!-- Proof of my devices belonging to me! -->
+              <reported-device />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import IncomingDevice from "@/components/Admin/Cards/IncomingDevice.vue";
+import ReportedDevice from "@/components/Admin/ReportedDevices.vue";
+import OwnedDeviceProof from "@/components/Admin/OwnedDeviceProof.vue";
+export default {
+  name: "TableDeviceOverview",
+  components: {
+    ReportedDevice,
+    OwnedDeviceProof,
+    IncomingDevice,
+  },
+  data() {
+    return {
+      openTab: 1,
+      devices: {
+        1: {
+          type: "computer",
+          date: "December-23-2021",
+          model: "Bionic Beaver 32",
+          name: "Mac Book Pro",
+          from: "James Harden",
+        },
+        2: {
+          type: "phone",
+          date: "December-23-2021",
+          model: "Bionic Beaver 32",
+          name: "Mac Book Pro",
+          from: "James Harden",
+        },
+        3: {
+          type: "electronic",
+          date: "December-23-2021",
+          model: "Bionic Beaver 32",
+          name: "Mac Book Pro",
+          from: "James Harden",
+        },
+      },
+    };
+  },
+  methods: {
+    toggleTabs: function (tabNumber) {
+      this.openTab = tabNumber;
+    },
+  },
+};
+</script>
