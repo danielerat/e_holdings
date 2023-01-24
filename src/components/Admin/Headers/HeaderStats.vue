2926b1fcc5685209896fab49b383d376de1d4c0f
izamha <glcode40@gmail.com>
2023-01-24 22:28:52 +0200

    Updates on HeaderStats

--- src/components/Admin/Headers/HeaderStats.vue
+++ src/components/Admin/Headers/HeaderStats.vue
@@ -9,11 +9,11 @@
         <div class="flex flex-wrap">
           <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
             <card-stats
-              statSubtitle="Sample"
-              statTitle="35,897"
-              statArrow="up"
-              statPercent="3.48"
-              statPercentColor="text-emerald-500"
+              statSubtitle="Devices"
+              :statTitle="devices.length"
+              statArrow="down"
+              statPercent="1.48"
+              statPercentColor="text-red-500"
               statDescripiron="This week"
               statIconName="chart-bar"
               statIconColor="bg-site-yellow-1"
@@ -21,11 +21,13 @@
           </div>
           <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
             <card-stats
-              statSubtitle="Sample USERS"
-              statTitle="2,356"
-              statArrow="down"
-              statPercent="3.48"
-              statPercentColor="text-red-500"
+              statSubtitle="Invoices"
+              :statTitle="invoices.length"
+              :statArrow="invoices.length !== 0 ? 'up' : 'down'"
+              statPercent="0.48"
+              :statPercentColor="
+                invoices.length !== 0 ? 'text-emerald-500' : 'text-red-500'
+              "
               statDescripiron="Since last week"
               statIconName="chart-pie"
               statIconColor="bg-site-gray-2"
@@ -33,11 +35,13 @@
           </div>
           <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
             <card-stats
-              statSubtitle="Sample"
-              statTitle="924"
-              statArrow="down"
-              statPercent="1.10"
-              statPercentColor="text-orange-500"
+              statSubtitle="Declared Lost"
+              :statTitle="lost.length"
+              :statArrow="lost.length !== 0 ? 'up' : 'down'"
+              statPercent="1"
+              :statPercentColor="
+                lost.length !== 0 ? 'text-emerald-500' : 'text-orange-500'
+              "
               statDescripiron="Yesterday"
               statIconName="users"
               statIconColor="bg-site-yellow-2"
@@ -45,11 +49,15 @@
           </div>
           <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
             <card-stats
-              statSubtitle="Sample"
-              statTitle="49,65%"
-              statArrow="up"
-              statPercent="12"
-              statPercentColor="text-emerald-500"
+              statSubtitle="Transfers"
+              :statTitle="pendingTransfers.length"
+              :statArrow="pendingTransfers.length !== 0 ? 'up' : 'down'"
+              statPercent="2"
+              :statPercentColor="
+                pendingTransfers.length !== 0
+                  ? 'text-emerald-500'
+                  : 'text-red-500'
+              "
               statDescripiron="Since last month"
               statIconName="percent"
               statIconColor="bg-site-green-2"
@@ -62,11 +70,25 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
 import CardStats from "@/components/Cards/CardStats.vue";
 
 export default {
   components: {
     CardStats,
+  },
+  computed: {
+    ...mapState({
+      devices: (state) => state.accountDevices,
+      invoices: (state) => state.accountInvoices,
+      lost: (state) => state.lostDevices,
+      pendingTransfers: (state) => state.pendingTransfers,
+    }),
+  },
+  created() {
+    this.$store.dispatch("fetchInvoices");
+    this.$store.dispatch("fetchDevicesPerAccount");
+    this.$store.dispatch("fetchLostDevices");
   },
 };
 </script>
